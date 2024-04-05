6e355d7fbbeeb999956692e239438c57a4d0af91
Erick Villaseñor <erickvillaseor@yahoo.com.mx>
2024-04-04 18:09:45 -0600

    Antes de agregar la api para login

---
+++ app/src/main/java/com/erick/geoapoyo/api/LoginApiClient.java
@@ -0,0 +1,9 @@
+package com.erick.geoapoyo.api;
+
+import retrofit2.Call;
+import retrofit2.http.POST;
+
+public class LoginApiClient {
+    @POST("login")
+    Call
+}
---
+++ app/src/main/java/com/erick/geoapoyo/api/RetrofitClient.java
@@ -0,0 +1,9 @@
+package com.erick.geoapoyo.api;
+
+import retrofit2.Retrofit;
+
+public class RetrofitClient {
+    public static Retrofit retrofit;
+
+    public static
+}
