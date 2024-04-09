360a4d843eac3cf6b49f0b6eb260e1c12745e690
Erick Villaseñor <erickvillaseor@yahoo.com.mx>
2024-04-09 09:15:22 -0600

    Api correcta

--- app/src/main/java/com/erick/geoapoyo/MainActivity.java
+++ app/src/main/java/com/erick/geoapoyo/MainActivity.java
@@ -2,7 +2,6 @@ package com.erick.geoapoyo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -21,43 +20,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Inicializar vistas
         editTextCorreo = findViewById(R.id.correo);
         editTextContrasenia = findViewById(R.id.contrasenia);
         botonIniciarSesion = findViewById(R.id.boton_iniciar_sesion);
 
-        // Inicializar ViewModel
         mViewModel = new ViewModelProvider(this).get(MainViewModel.class);
 
-        // Observar el evento de inicio de sesión exitoso
         mViewModel.getLoginSuccessEvent().observe(this, loginSuccess -> {
             if (loginSuccess) {
-                // Mostrar Toast de inicio de sesión exitoso
                 Toast.makeText(MainActivity.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
-                // Iniciar la pantalla de HomePage
                 startActivity(new Intent(MainActivity.this, HomePage.class));
-                finish(); // Opcional: cerrar la actividad actual si ya no es necesaria
+                finish();
             }
         });
 
         botonIniciarSesion.setOnClickListener(view -> {
-            // Obtener valores del correo y la contraseña
             String correo = editTextCorreo.getText().toString().trim();
             String contrasenia = editTextContrasenia.getText().toString().trim();
 
-            // Verificar si el correo está vacío
             if (correo.isEmpty()) {
                 Toast.makeText(MainActivity.this, "Ingrese su correo", Toast.LENGTH_SHORT).show();
-                return; // Salir del método para evitar la ejecución adicional
+                return;
             }
 
-            // Verificar si la contraseña está vacía
             if (contrasenia.isEmpty()) {
                 Toast.makeText(MainActivity.this, "Ingrese su contraseña", Toast.LENGTH_SHORT).show();
-                return; // Salir del método para evitar la ejecución adicional
+                return;
             }
 
-            // Intentar iniciar sesión
             mViewModel.login(correo, contrasenia);
         });
     }
